0abaf74e3701becb9b7f31d14a7d005fa3ebd532
Marco Ballario <marco.ballario1997@gmail.com>
2022-07-07 15:39:02 +0200

    Create discovery-service module

---
+++ discovery-service/src/test/kotlin/it/polito/wa2/g12/discoveryservice/DiscoveryServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package it.polito.wa2.g12.discoveryservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
